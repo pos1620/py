6b0db3da73978f8f7e6f4f5e243cea4001bd7e4e
pos1620 <pos1620@outlook.com>
2019-01-22 09:35:19 -0200

    written by alxsy,updtin

--- getSystem.py
+++ getSystem.py
@@ -1,4 +1,7 @@
 import os;
-print "operation System:",os.name
+print "operation System:"+os.name
 os.system("clear");
-os.system("start .");
+#os.system("start .");
+#os.system("rm "+os.name)
+f=open(os.name+".txt","w")
+f.write("alxsy\nwelcome")
