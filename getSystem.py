1e621e534b3b4b12de6130e39eb42724435b53d8
pos1620 <pos1620@outlook.com>
2019-01-23 07:14:37 -0200

    written by alxsy,updtin gitClone

--- getSystem.py
+++ getSystem.py
@@ -1,4 +1,8 @@
 import os;
+#import sys;#sys.stdin.readlines()
+#data = sys.stdin.readlines()
+#print "Counted", len(data), "lines."
+#name = raw_input("Enter your name: ") 
 print "operation System:"+os.name
 os.system("clear");
 #os.system("start .");
