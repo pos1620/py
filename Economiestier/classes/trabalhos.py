19a572edae9f0c832c302228e95542fcf9a098fe
pos1620 <pos1620@outlook.com>
2019-02-05 08:50:15 -0200

    written by alxsy,updtin economiestier creating trabalhos class and methods get and set ter,  5 de fev de 2019 08:50:15

--- Economiestier/classes/trabalhos.py
+++ Economiestier/classes/trabalhos.py
@@ -1,6 +1,34 @@
 class trabalhos:
-    valor;
-    termino;
-    inicio;
-    tempo;
+    valor=None;
+    termino=None;
+    inicio=None;
+    tempo=None;
 
+
+
+    def setValor(self,valor):
+        self.valor=self;
+
+    def getValor(self):
+        return self.valor
+
+
+    def setTermino(self,termino):
+        self.termino=termino;
+
+    def getTermino(self):
+        return self.termino;
+
+
+    def setInicio(self,inicio):
+        self.inicio=inicio;
+
+    def getInicio(self):
+        return self.inicio;
+
+
+    def setTempo(self,tempo):
+        self.tempo=tempo
+
+    def getTempoo(self):
+        return self.tempo
